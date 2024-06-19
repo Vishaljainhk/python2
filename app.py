c16fa926ae5a7884d840fdf6c914387f41a52695
Vishaljainhk <91910508+Vishaljainhk@users.noreply.github.com>
2024-06-19 11:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello welcome to my python page"
+    return "Hello, welcome to my python page"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
